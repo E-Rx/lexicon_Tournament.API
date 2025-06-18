16cac8bfd73635038f420723090d7f6e93fb2032
E-Rx <elise.rochaix@gmail.com>
2025-06-18 13:50:37 +0200

    entities / controllers added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Tournament.Data.Data;
+{
+    
+}
 
 namespace Tournament.API
 {
@@ -6,10 +12,15 @@ namespace Tournament.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<TournamentAPIContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TournamentAPIContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIContext' not found.")));
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+                .AddNewtonsoftJson()
+                .AddXmlSerializerFormatters();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
